b171e4962639fbbf8611f27574568971b57d8536
Otari Maglaperidze <oticko@yandex.ru>
2021-04-25 10:29:34 +0300

    add cli to braingames

--- brain_games/scripts/braingames.py
+++ brain_games/scripts/braingames.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+from brain_games import cli
+
 
 def greet(welcome):
     print(welcome)
 
+
 def main():
     greet('Welcome to the Brain Games!')
+    cli.welcome_greet()
+
 
 if __name__ == '__main__':
     main()
