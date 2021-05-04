ae336b54728a6e2e524a771f716db81cb76ad0c8
Otari Maglaperidze <oticko@yandex.ru>
2021-05-04 17:09:44 +0300

    add counter_answer.py

---
+++ brain_games/scripts/counter_answer.py
@@ -0,0 +1,22 @@
+def counter(question, name):
+    count = 0
+    while count < 3:
+        result = question()
+        if result is True:
+            count += 1
+        else:
+            break
+    if count == 3:
+        print('Congratulations, {}!'.format(name))
+    else:
+        (print('{} is wrong answer ;(. Correct answer was {}'.
+               format(result[1], result[0])))
+        print("Let's try again, {}!".format(name))
+
+
+def main():
+    counter()
+
+
+if __name__ == '__main__':
+    main()
