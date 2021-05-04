2f53e728262a99e752c332c5434e7c436d358000
Otari Maglaperidze <oticko@yandex.ru>
2021-05-05 01:02:22 +0300

    modify welcome_speech

--- brain_games/scripts/welcome_speech.py
+++ brain_games/scripts/welcome_speech.py
@@ -5,7 +5,6 @@ def welcome_speech():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
-    print('Answer "yes" if the number is even, otherwise answer "no".')
     return name
 
 
