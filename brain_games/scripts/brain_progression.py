e26a74e8d79d9d4aacf47fa17efdfa2bf56fa33c
Otari Maglaperidze <oticko@yandex.ru>
2021-05-14 01:02:04 +0300

    change main logic in brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,31 +1,33 @@
-import prompt
 import random
-from brain_games.scripts import counter_answer
-from brain_games.scripts import welcome_speech
+from brain_games.games import all_games
+from brain_games.games import games_engine
 
 
 def find_progression_elem():
+    #Длина прогрессии по условиям задачи от 5 до 10 элементов
     len_progression = random.randint(5, 10)
+    #Первый элемент прогрессии случайное число от 0 до 100
     first_number = random.randint(0, 100)
+    #Шаг прогрессии от 1 до 10
     progressions_step = random.randint(1, 10)
+    #Заполняем первый элемент прогрессии
     progression = [first_number]
-    for elem in range(1, len_progression):
+    #Шагаем по прогресси, начиная с первого элемента
+    steps = 1
+    while steps < len_progression:
         first_number += progressions_step
         progression.append(first_number)
+        steps += 1
+    #Определяем случайно место пропущенного элемента последовательности
     random_dots = random.randint(0, len_progression - 1)
     correct_answer = progression[random_dots]
     progression[random_dots] = '..'
-    print('Question:', *progression)
-    user_answer = prompt.string('Your answer: ')
-    if str(correct_answer) == user_answer:
-        return True
-    return str(correct_answer), user_answer
+    return correct_answer, progression
 
 
 def main():
-    name = welcome_speech.welcome_speech()
-    print('What number is missing in the progression?')
-    counter_answer.counter(find_progression_elem, name)
+    games_question = 'What number is missing in the progression?'
+    games_engine.counter(find_progression_elem, games_question)
 
 
 if __name__ == '__main__':
