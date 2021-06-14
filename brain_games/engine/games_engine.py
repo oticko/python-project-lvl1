7ac2d32e7abf55babbf93be88d4169dffcf07d50
Otari Maglaperidze <oticko@yandex.ru>
2021-06-14 18:51:46 +0300

    move games_engine.py to new folder

--- brain_games/engine/games_engine.py
+++ brain_games/engine/games_engine.py
@@ -1,4 +1,5 @@
 import prompt
+NUMBER_OF_ROUNDS = 3
 
 
 def welcome_speech(games_question):
@@ -11,26 +12,16 @@ def welcome_speech(games_question):
 
 def handler(game, games_question):
     name = welcome_speech(games_question)
-    current_round = 0
-    number_of_rounds = 3
-    while current_round < number_of_rounds:
+    for current_round in range(0, NUMBER_OF_ROUNDS):
         problem, correct_answer = game()
-        print('Question:', *problem)
+        correct_answer = str(correct_answer)
+        print('Question:', problem)
         user_answer = prompt.string('Your answer: ')
-        if str(correct_answer) != user_answer:
+        if correct_answer != user_answer:
             (print('{!r} is wrong answer ;(. Correct answer was {!r}.'.
                    format(user_answer, correct_answer)))
             print("Let's try again, {}!".format(name))
             break
         print('Correct!')
-        current_round += 1
-    if current_round == number_of_rounds:
+    if correct_answer == user_answer:
         print('Congratulations, {}!'.format(name))
-
-
-def main():
-    handler()
-
-
-if __name__ == '__main__':
-    main()
