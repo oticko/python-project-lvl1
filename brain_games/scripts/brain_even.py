420fe737e6b66cc321d8841b0806a6d20dca8c37
Otari Maglaperidze <oticko@yandex.ru>
2021-06-15 20:07:07 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,7 +3,8 @@ from brain_games.games import brain_even
 
 
 def main():
-    games_engine.handler(brain_even.problems_maker, brain_even.GAMES_QUESTION)
+    games_engine.handler(brain_even.make_problem_with_solve,
+                         brain_even.GAMES_QUESTION)
 
 
 if __name__ == '__main__':
